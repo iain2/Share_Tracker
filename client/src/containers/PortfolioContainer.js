0f2c88abd50a6eedf4261aed398a587cfb9c88e3
iain2 <iain.a.hay65@gmail.com>
2022-07-01 14:54:24 +0100

    services files added

--- client/src/containers/PortfolioContainer.js
+++ client/src/containers/PortfolioContainer.js
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NavBar from "../components/NavBar";
 import UserPortfolio from "../components/UserPortfolio";
+import { getPortfolio } from '../PortfolioService'
 
 const PortfolioContainer = () => {
+
+    const [portfolio, setPortfolio] = useEffect([]);
+
+    useEffect(() => {
+        getPortfolio().then((allStocks) => {
+            setPortfolio(allStocks);
+        })
+    });
+
 
 
     return (
         <>
             <h1>PortfolioContainer</h1>
             <NavBar />
-            <UserPortfolio />
+            <UserPortfolio portfolio={portfolio} />
 
         </>
     )

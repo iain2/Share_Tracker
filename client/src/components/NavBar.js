6c42bf9e9a47e3b90ecd2387be517f4644cad8e8
NadiaA14 <nadia.almurshedy@hotmail.com>
2022-07-04 10:28:53 +0100

    navbar css

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,10 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from 'styled-components' 
+
+const NavBarContainer = styled.nav`
+    color: white;
+  text-align: left;
+  background: #556f7a;
+  width: 100%;
+  ul {
+    color: #ffffff;
+    display: flex;
+    flex-direction: row;
+    border: 1px solid green;
+    list-style: none;
+    li {
+      padding: 15px 15px;
+      border: 2px #ffffff;
+    }
+    a {
+        color: #ffffff;
+        text-decoration: none;
+    }
+}
+`
 
 const NavBar = () => {
 
 
     return (
+        <NavBarContainer>
         <ul>
             <li>
                 <Link to="/">Home</Link>
@@ -12,9 +36,8 @@ const NavBar = () => {
             <li>
                 <Link to="/shares">Shares</Link>
             </li>
-
-
         </ul>
+        </NavBarContainer>
     )
 };
 
